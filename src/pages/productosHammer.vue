39d1b40c927366a6d083fe659180260018a0a07d
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-02-12 00:12:47 +0100

    filtro pesos

--- src/pages/productosHammer.vue
+++ src/pages/productosHammer.vue
@@ -2,19 +2,24 @@
   <main>
     <div class="marca"><h1>HAMMER</h1></div>
     <div>
-      <input type="radio" id="huey" name="peso"
-             checked>
+      <input v-model="peso" type="radio" value="pesoLibre" name="peso">
       <label >Peso Libre</label>
     </div>
     <div>
-      <input type="radio" name="peso">
+      <input v-model="peso" type="radio" value="pesoGuiado" name="peso">
       <label >Peso Guiado</label>
     </div>
 
     <div class="productos">
-        <div class="producto" v-for="producto in productos">
-          <Producto v-show="producto.category==='peso guiado'" :producto="producto"/>
-        </div>
+      <div v-if="this.peso==='pesoLibre'" class="producto" v-for="producto in productosPesoLibre">
+        <Producto :producto="producto"/>
+      </div>
+      <div v-else-if="this.peso==='pesoGuiado'" class="producto" v-for="producto in productosPesoGuiado">
+        <Producto :producto="producto"/>
+      </div>
+      <div v-else class="producto" v-for="producto in productos">
+        <Producto :producto="producto"/>
+      </div>
     </div>
 
   </main>
@@ -32,6 +37,9 @@ export default {
   data() {
     return {
       productos:null,
+      productosPesoLibre:null,
+      productosPesoGuiado:null,
+      peso:null,
     }
   },
   methods: {
@@ -43,7 +51,20 @@ export default {
           .catch((e) => {
             console.log(e)
           })
-      debugger
+      gymApi.get(`productos/marca/${this.$route.params.marca}/peso-guiado`)
+          .then(res => {
+            this.productosPesoGuiado = res.data
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+      gymApi.get(`productos/marca/${this.$route.params.marca}/peso-libre`)
+          .then(res => {
+            this.productosPesoLibre = res.data
+          })
+          .catch((e) => {
+            console.log(e)
+          })
     }
   }
   ,
