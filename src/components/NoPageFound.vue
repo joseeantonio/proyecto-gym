3d97d0660cf8635a9770406dc54f6530696d7ace
jose <jagonzalez.alumno@iesrafaelalberti.es>
2023-02-19 22:59:05 +0100

    error 404

--- src/components/NoPageFound.vue
+++ src/components/NoPageFound.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>No page found</h1>
+  <main>
+    <div>
+      <h1>ERROR 404</h1>
+      <p>Pagina no encontrada</p>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -10,5 +15,18 @@ export default {
 
 
 <style scoped>
-
+main{
+  min-height: 700px;
+  background-color: rgba(0, 0, 0, 0.61);
+  display: flex;
+  justify-content: center;
+}
+h1{
+  font-size: 100px;
+  font-weight: bold;
+}
+div{
+  margin: auto;
+  text-align: center;
+}
 </style>
